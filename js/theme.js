e3302eea474d8c2299c395ff9c5627dbced0ab74
impaladin69 <70229533+impaladin69@users.noreply.github.com>
2020-08-26 05:23:50 +0530

    All files added

---
+++ js/theme.js
@@ -0,0 +1,29 @@
+/*
+================================================================
+* Template:  	 DomainX - Domain for Sale Html Template
+* Written by: 	 Harnish Design - (http://www.harnishdesign.net)
+* Description:   Main Custom Script File
+================================================================
+*/
+
+
+(function ($) {
+	"use strict";
+
+// Preloader
+$(window).on('load', function () {
+	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
+	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(333);
+});
+
+
+/*------------------------
+   tooltips
+-------------------------- */
+$('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
+
+
+
+
+})(jQuery)
